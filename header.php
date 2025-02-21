b5827c8a7f4e5ce0830e594dcbb75a2c770aa3d5
jordan84292 <gonzalezjordan61@gmail.com>
2025-02-20 22:42:00 -0600

    Menu

---
+++ header.php
@@ -0,0 +1,23 @@
+<header>
+    <div class="headerUp">
+        <div class="logo"><a href="#"><img src="./imgs/logo.png" alt="logo-star"></a></div>
+    </div>
+    <div class="headerDown">
+        <ul>
+            <li><a href="#"></a>News</li>
+            <li><a href="#"></a>Discord Webhook</li>
+            <li><a href="#"></a>Database</li>
+            <li><a href="#"></a>Tools</li>
+            <li><a href="#"></a>Guides</li>
+            <li><a href="#"></a>Community</li>
+            <li><a href="#"></a>Premium</li>
+        </ul>
+        
+        <div class="social">
+        <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+        <i class="fa-solid fa-bell"></i>
+        <i class="fa-solid fa-earth-americas"></i>
+        <i class="fa-solid fa-envelope"></i>
+        </div>
+    </div>
+</header>
